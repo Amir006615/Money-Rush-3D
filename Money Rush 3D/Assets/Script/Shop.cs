e2e96417fb364865be4beeb4fa15aa13b3349e53
Amir006615 <109579072+Amir006615@users.noreply.github.com>
2022-07-20 01:25:32 -0700

    Alireza5

--- Money Rush 3D/Assets/Script/Shop.cs
+++ Money Rush 3D/Assets/Script/Shop.cs
@@ -1,9 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Shop : MonoBehaviour
 {
-   
+    [System.Serializable] class ShopItem
+    {
+        public Sprite Image;
+        public int price;
+        public bool IsPurchased = false;
+    }
+
+    [SerializeField] List<ShopItem> ShopItemsList;
+
+    GameObject ItemTemplate;
+    GameObject g;
+    [SerializeField] Transform ShopScrollView;
+
+    void Start()
+    {
+        ItemTemplate = ShopScrollView.GetChild(0).gameObject;
+
+        int len = ShopItemsList.Count;
+        for (int i = 0; i < len; i++)
+        {
+            g = Instantiate(ItemTemplate, ShopScrollView);
+            g.transform.GetChild(0).GetComponent<Image>().sprite = ShopItemsList[i].Image;
+            g.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = ShopItemsList[i].price.ToString();
+            g.transform.GetChild(2).GetComponent<Button>().interactable = !ShopItemsList[i].IsPurchased;
+        }
+
+        Destroy(ItemTemplate);
+    }
 }
